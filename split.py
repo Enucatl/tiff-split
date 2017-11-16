8a60be341a398fd76c842552a118296301fb27ab
Matteo Abis <gmatteo..abis@gmail.com>
2017-11-16 14:26:34 +0100

    starting

--- split.py
+++ split.py
@@ -1,13 +1,18 @@
+import os
 import click
 import numpy as np
 import libtiff
 import glob
+from tqdm import tqdm
 
 @click.command()
 @click.option("-n", type=int, default=5)
 @click.argument("folder", type=click.Path(exists=True))
 def main(n, folder):
-    pass
+    input_filenames = glob.glob(os.path.join(folder, "*.tif"))
+    for f in tqdm(input_filenames):
+        tif = libtiff.TIFF.open(f).read_image()
+        print(tif.shape)
 
 
 if __name__ == "__main__":
